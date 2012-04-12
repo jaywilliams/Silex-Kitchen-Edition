2f68720db15423c065f5fcdb983c640a2cb3b3b7
Jay Williams <jay@myd3.com>
2012-04-12 16:32:12 -0500

    Remove Assetic Extension & Add Bootstrap v2 Starter Page

--- src/config.php
+++ src/config.php
@@ -11,30 +11,11 @@ $app['db.config.password']  = '';
 $app['debug'] = true;
 
 // Local
-$app['locale'] = 'fr';
+$app['locale'] = 'en';
 $app['session.default_locale'] = $app['locale'];
-$app['translator.messages'] = array(
-    'fr' => __DIR__.'/../resources/locales/fr.yml',
-);
 
 // Cache
 $app['cache.path'] = __DIR__ . '/../cache';
 
 // Http cache
 $app['http_cache.cache_dir'] = $app['cache.path'] . '/http';
-
-// Assetic
-$app['assetic.path_to_cache']       = $app['cache.path'] . DIRECTORY_SEPARATOR . 'assetic' ;
-$app['assetic.path_to_web']         = __DIR__ . '/../web/assets';
-
-$app['assetic.input.path_to_assets']    = __DIR__ . '/../resources/assets';
-$app['assetic.input.path_to_css']       = $app['assetic.input.path_to_assets'] . '/css/*.css';
-$app['assetic.output.path_to_css']      = '/css/styles.css';
-$app['assetic.input.path_to_js']        = array(
-    $app['assetic.input.path_to_assets'] . '/js/bootstrap-twipsy.js',
-    $app['assetic.input.path_to_assets'] . '/js/bootstrap-*.js',
-    $app['assetic.input.path_to_assets'] . '/js/script.js',
-);
-$app['assetic.output.path_to_js']       = '/js/scripts.js';
-
-$app['assetic.filter.yui_compressor.path'] = '/usr/share/yui-compressor/yui-compressor.jar';
